2bf463b82dcab034d574b025ef8be042ddc780ea
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-02-11 22:43:32 +0100

    extra pages add

---
+++ app/src/main/java/com/example/authentication_app/landingPage.java
@@ -0,0 +1,14 @@
+package com.example.authentication_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class landingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
