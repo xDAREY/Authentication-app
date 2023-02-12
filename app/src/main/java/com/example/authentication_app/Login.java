e8693f987c918fc2800787e9a87825e6836ad2be
Oluwadare Emmanuel <oluwadare.emmanuel15@gmail.com>
2023-02-12 20:13:39 +0100

    dB added

--- app/src/main/java/com/example/authentication_app/Login.java
+++ app/src/main/java/com/example/authentication_app/Login.java
@@ -2,13 +2,54 @@ package com.example.authentication_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.authentication_app.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    DataBaseCon dataBaseCon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        dataBaseCon = new DataBaseCon(this);
+
+        binding.loginBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = binding.inputUsername.getText().toString();
+                String password = binding.inputPassword.getEditText().getText().toString();
+
+                if (username.equals("") || password.equals(""))
+                    Toast.makeText(Login.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkDetails = dataBaseCon.checkEmailPassword(username, password);
+
+                    if (checkDetails == true){
+                        Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), landingPage.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Login.this, "Invalid details", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        binding.signUpRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/authentication_app/SignUp.java
+++ app/src/main/java/com/example/authentication_app/SignUp.java
@@ -2,13 +2,70 @@ package com.example.authentication_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.authentication_app.databinding.ActivitySignUpBinding;
 
 public class SignUp extends AppCompatActivity {
 
+    ActivitySignUpBinding binding;
+    DataBaseCon DbCon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        binding =ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        DbCon = new DataBaseCon(this);
+
+        binding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.hintEmail.getText().toString();
+                String password = binding.inputPassword.getEditText().getText().toString();
+                String confirmPass = binding.inputRePassword.getEditText().getText().toString();
+
+                if (email.equals("") || password.equals("") || confirmPass.equals(""))
+                    Toast.makeText(SignUp.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
+                else {
+                    if (password.equals(confirmPass)) {
+                        Boolean checkUserEmail = DbCon.checkEmail(email);
+
+                        if (checkUserEmail == false) {
+                            Boolean insert = DbCon.insertData(email, password);
+
+                            if (insert == true) {
+                                Toast.makeText(SignUp.this, "Account was created successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), Login.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(SignUp.this, "Failed to create an account", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(SignUp.this, "User already exists", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(SignUp.this, "Invalid password", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+        binding.redirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
     }
 }
